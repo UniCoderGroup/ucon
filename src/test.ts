987f19f676df4eacec6e74b1bcb5f0b9d3a62d8b
KermanX <kermanx@qq.com>
2021-12-08 18:55:32 +0800

    update

--- src/test.ts
+++ src/test.ts
@@ -1,22 +1,59 @@
-import { ucon, ProgressBar } from "./index";
-import { stdout } from 'process';
-const p1 = new ProgressBar({ width: 20, name: "Processing1" });
-const p2 = new ProgressBar({ width: 20, name: "Processing2" });
-(async()=>{
-p1.mount();
-p2.mount();
-})();
-ucon.log("1","2","3");
-ucon.log("1","2","3");
-ucon.log("1","2","3");
-ucon.log("1","2","3");
-let n = 0;
-const timer = setInterval(async () => {
-  if (n > 19) return;
-  p1.progress(0.1);
-  p2.progress(0.05);
-  n++;
-}, 1000);
+import { ucon, ProgressBar, Table ,GroupBox} from "./index";
+
+let group = new GroupBox({});
+
+
+// interface Process{
+//   Id:number,
+//   ProcessName:string
+// };
+// let table = new Table<Process>({
+//   title:"Process",
+//   separator:true,
+//   cols:[
+//     {
+//       title:"PID",
+//       width: 10,
+//       align: "middle",
+//       key:"Id"
+//     },
+//     {
+//       title:"Name",
+//       width: 50,
+//       align: "left",
+//       key:"ProcessName"
+//     }
+//   ]
+// });
+// table.datas=[
+//   {
+//     Id:19728,
+//     ProcessName:"ApplicationFrameHost"
+//   },
+//   {
+//     Id:21172,
+//     ProcessName:"audiodg"    
+//   }
+// ]
+// table.mount();
+
+// const p1 = new ProgressBar({ width: 20, name: "Processing1",fractionDigits:3 });
+// const p2 = new ProgressBar({ width: 20, name: "Processing2",fractionDigits:3 });
+// (async()=>{
+// p1.mount();
+// p2.mount();
+// })();
+// ucon.log("1","2","3");
+// ucon.log("1","2","3");
+// ucon.log("1","2","3");
+// ucon.log("1","2","3");
+// let n = 0;
+// const timer = setInterval(async () => {
+//   if (n > 19) return;
+//   p1.progress(0.1);
+//   p2.progress(0.05);
+//   n++;
+// }, 1000);
 
 // stdout.write("1\n");
 // stdout.write("1\n");
