b8afc54b1d93304e61bc0bbbf2fb1d27757b2347
KermanX <kermanx@qq.com>
2021-12-17 12:30:35 +0800

    finish dividing.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
-import { UCon } from "./ucon";
+export * from "./global";
+export * from "./line";
+export * from "./ucon";
+export * from "./utty";
+export * from "./components";
+export * from "./std_components";
 
+import { UCon } from "./ucon";
 export const ucon = new UCon();
