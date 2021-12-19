61e034b25e388b25563629a50b19a8047e81d881
_Kerman <kermanx@qq.com>
2021-12-19 15:16:30 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ export * from "./global";
 export * from "./line";
 export * from "./ucon";
 export * from "utty";
-export * from "./components";
+export * from "./component";
 export * from "./std_components";
 
 import { UCon } from "./ucon";
