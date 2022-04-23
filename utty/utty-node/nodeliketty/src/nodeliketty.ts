0dd9d72333ec265cf019d063e1f7a9406e64d52c
_Kerman <KermanX@qq.com>
2022-04-23 13:49:28 +0800

    edit commits

--- utty/utty-node/nodeliketty/src/nodeliketty.ts
+++ utty/utty-node/nodeliketty/src/nodeliketty.ts
@@ -50,24 +50,6 @@ export default interface NodeLikeTty {
    * * `4` for 16,
    * * `8` for 256,
    * * `24` for 16,777,216 colors supported.
-   *
-   * Use this to determine what colors the terminal supports. Due to the nature of
-   * colors in terminals it is possible to either have false positives or false
-   * negatives. It depends on process information and the environment variables that
-   * may lie about what terminal is used.
-   * It is possible to pass in an `env` object to simulate the usage of a specific
-   * terminal. This can be useful to check how specific environment settings behave.
-   *
-   * To enforce a specific color support, use one of the below environment settings.
-   *
-   * * 2 colors: `FORCE_COLOR = 0` (Disables colors)
-   * * 16 colors: `FORCE_COLOR = 1`
-   * * 256 colors: `FORCE_COLOR = 2`
-   * * 16,777,216 colors: `FORCE_COLOR = 3`
-   *
-   * Disabling color support is also possible by using the `NO_COLOR` and`NODE_DISABLE_COLORS` environment variables.
-   * @since v9.9.0
-   * @param [env=process.env] An object containing the environment variables to check. This enables simulating the usage of a specific terminal.
    */
   getColorDepth(): number;
 }
